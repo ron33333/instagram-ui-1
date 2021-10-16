76bb246b697e22f3a522269217c4e91c903efe1d
Michael Baliasny <michael.baliasny@gmail.com>
2021-10-16 21:40:14 +0300

    homework commit- check if mail exists

--- src/Register/register.schema.js
+++ src/Register/register.schema.js
@@ -1,4 +1,5 @@
 import * as yup from 'yup';
+import {checkAvailabilityEmail} from '../services/user.service'
 
 export const registerSchema = yup.object().shape({
     username: yup.string()
@@ -6,7 +7,13 @@ export const registerSchema = yup.object().shape({
         .required(),
     email: yup.string()
         .email()
-        .required(),
+        .required()
+        .test("email",'This email has already been registered',async function (email) 
+            {
+                const temp = await checkAvailabilityEmail(email);
+                console.log(temp);
+                return await checkAvailabilityEmail(email);
+            }),
     password: yup.string()
         .required()
         .min(6)
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,7 +1,8 @@
-import {config} from '../config/index';
+import {development} from '../config/development';
+
 export async function register(user)
 {
-    const res = await fetch(`${config.apiUrl}/user/`,
+    const res = await fetch(`${development.apiUrl}/user/`,
         {
             method:'POST',
             body:JSON.stringify(user),
@@ -12,4 +13,31 @@ export async function register(user)
         });
     return res.json();
 }
+
+export async function checkAvailabilityEmail(email) {
+    const res = await fetch(`${development.apiUrl}/user/get`)
+    .then((res)=> res.json())
+    .then((users)=> {const isValid = users.find(user=>user.email === email);
+        return isValid;
+    })
+    .catch(false);
+    return !res;
     
+    // const arr = res.json();
+    // let condition = true;
+    // for(var i=0;i<arr.length;i++)
+    // {
+    //     if(arr[i].email === email){
+    //         condition = !condition;
+    //     }
+    // }
+    
+    // return condition;
+    // console.log(res);
+    // return false;
+}
+
+// module.exports={
+//     register,
+//     checkAvailabilityEmail
+// }   
